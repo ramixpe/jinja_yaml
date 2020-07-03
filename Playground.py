73e118816eea4c2066123f4d725d61915b63e0e2
ramixpe <30420208+ramixpe@users.noreply.github.com>
2020-07-04 02:40:25 +0300

    Update Playground.py

--- Playground.py
+++ Playground.py
@@ -1,30 +1,9 @@
 
-# import csv
-# from jinja2 import Template
-# source_file = 'book2.csv'
-# interface_template_file = 'template.j2'
-
-# with open(interface_template_file) as f:
-#     interface_template = Template(f.read(),keep_trailing_newline=True)
-
-# interafce_configs = ""
-# with open(source_file) as f:
-#     reader = csv.DictReader(f)
-#     for raw in reader:
-#         interface_config = interface_template.render(
-#             interface = raw['interface'],
-#             description = raw['description'],
-#             address = raw['address']
-#         )
-#         interafce_configs += interface_config
-
-# with open('configuration_interfaces.txt', 'w') as f:
-#     f.write(interafce_configs)
 
 from jinja2 import Environment, FileSystemLoader
 import yaml
 import os
-from pprint import pprint
+
 
 with open("source.yaml", 'r') as stream:
     Devices = (yaml.safe_load(stream))
